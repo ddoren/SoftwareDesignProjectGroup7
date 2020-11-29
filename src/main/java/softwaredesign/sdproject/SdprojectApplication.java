b88456fed444a9019243980468481689f1550fac
Dorin Moldovan <45043854+ddoren@users.noreply.github.com>
2020-11-29 17:05:15 +0100

    Main Back-End

---
+++ src/main/java/softwaredesign/sdproject/SdprojectApplication.java
@@ -0,0 +1,13 @@
+package softwaredesign.sdproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/softwaredesign/sdproject/SdprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package softwaredesign.sdproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
