ba8017f73455613f6ade653d38bdc3fec34364cf
Dorin <jtrettore@gmail.com>
2020-11-30 15:31:02 +0100

    Controller Implemented; minor changes

--- src/main/java/softwaredesign/sdproject/controller/ArticleController.java
+++ src/main/java/softwaredesign/sdproject/controller/ArticleController.java
@@ -1,35 +1,35 @@
-package softwaredesign.sdproject.controller;
-
-
-import softwaredesign.sdproject.model.Article;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import softwaredesign.sdproject.repository.ArticleRepository;
-import softwaredesign.sdproject.repository.CommentRepository;
-
-import java.util.ArrayList;
-import java.util.List;
-
-@RestController
-public class ArticleController {
-
-    @Autowired
-    ArticleRepository articleRepository;
-
-    @Autowired
-    CommentRepository commentRepository;
-
-    //All Articles
-    @GetMapping("/articles")
-    public ResponseEntity<List<Article>> getAllArticles(@RequestParam(required = false) String id) {
-        List<Article> articles = new ArrayList<>();
-        articles = articleRepository.findAll();
-        return new ResponseEntity<>(articles, HttpStatus.OK);
-    }
-
-
-}
+//package softwaredesign.sdproject.controller;
+//
+//
+//import softwaredesign.sdproject.model.Article;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
+//import softwaredesign.sdproject.repository.ArticleRepository;
+//import softwaredesign.sdproject.repository.CommentRepository;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//@RestController
+//public class ArticleController {
+//
+//    @Autowired
+//    ArticleRepository articleRepository;
+//
+//    @Autowired
+//    CommentRepository commentRepository;
+//
+//    //All Articles
+//    @GetMapping("/articles")
+//    public ResponseEntity<List<Article>> getAllArticles(@RequestParam(required = false) String id) {
+//        List<Article> articles = new ArrayList<>();
+//        articles = articleRepository.findAll();
+//        return new ResponseEntity<>(articles, HttpStatus.OK);
+//    }
+//
+//
+//}
---
+++ src/main/java/softwaredesign/sdproject/controller/ArticleControllerNR.java
@@ -0,0 +1,27 @@
+package softwaredesign.sdproject.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import softwaredesign.sdproject.model.Article;
+import softwaredesign.sdproject.repository.ArticleRepository;
+
+import java.util.List;
+
+@Controller
+public class ArticleControllerNR {
+
+    @Autowired
+    ArticleRepository articleRepository;
+
+    //Show All Articles
+    @GetMapping("/articles")
+    public String getAllArticles(Model model){
+        List<Article> articleList = articleRepository.findAll();
+        model.addAttribute("articles", articleList);
+        return "/ShowAricles.html";
+    }
+
+
+}
